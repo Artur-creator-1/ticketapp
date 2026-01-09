81dca079be285dfe5f6dc546cc82c36cb3d3f459
ilya <ii7737918@gmail.com>
2026-01-09 21:15:23 +0300

    userservice done

---
+++ backend/userservice/src/main/java/com/example/userservice/constant/Constants.java
@@ -0,0 +1,6 @@
+package com.example.userservice.constant;
+
+public class Constants {
+    public static final String COMPANY_NAME = "Company name";
+    public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
+}
---
+++ backend/userservice/src/main/java/com/example/userservice/service/UserService.java
@@ -0,0 +1,52 @@
+package com.example.userservice.service;
+
+import com.example.userservice.model.Credential;
+import com.example.userservice.model.Device;
+import com.example.userservice.model.User;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface UserService {
+    User getUserByEmail(String email);
+    User getUserByUuid(String userUuid);
+    User updateUser(String userUuid, String firstName, String lastName, String email, String phone, String bio, String address);
+    void createUser(String firstName, String lastName, String email, String username, String password);
+    void verifyAccount(String token);
+    User verifyPasswordToken(String token);
+    User enableMfa(String userUuid);
+    User disableMfa(String userUuid);
+    User uploadPhoto(String userUuid, MultipartFile file);
+    User toggleAccountExpired(String userUuid);
+    User toggleAccountLocked(String userUuid);
+    User toggleAccountEnabled(String userUuid);
+    User toggleCredentialsExpired(String userUuid);
+    void updatePassword(String userUuid, String currentPassword, String newPassword, String confirmNewPassword);
+    User updateRole(String userUuid, String role);
+    void resetPassword(String email);
+    void doResetPassword(String userUuid, String token, String password, String confirmPassword);
+    List<User> getUsers();
+    User getAssignee(String ticketUuid);
+    Credential getCredential(String userUuid);
+    List<Device> getDevices(String userUuid);
+    List<User> getTechSupports();
+    User getTicketUser(String ticketUuid);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
